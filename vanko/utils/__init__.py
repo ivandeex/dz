370439cbee1ec65c08bd2e30b9a2e5d8dbcf9db5
ivandeex <ivandeex@gmail.com>
2016-04-14 16:20:59 +0300

    fix utils shortcuts: dates

--- vanko/utils/__init__.py
+++ vanko/utils/__init__.py
@@ -1,3 +1,3 @@
 from .misc import getenv, randsleep, result_as_list
 from .decode import encode_userpass, decode_userpass
-from .datetime import extract_datetime
+from .dates import extract_datetime
