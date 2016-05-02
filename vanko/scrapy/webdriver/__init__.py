f02a9a1297157db5c367b612f6b10232055c2f00
vanko <vanko@vanko.me>
2016-05-03 02:11:54 +0300

    remove vanko here

--- vanko/scrapy/webdriver/__init__.py
+++
@@ -1,3 +0,0 @@
-from .spider import WebdriverSpider
-from .http import WebdriverRequest, WebdriverActionRequest, WebdriverResponse
-from .middlewares import WebdriverSpiderMiddleware
--- vanko/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .misc import getenv, randsleep, result_as_list
-from .decode import encode_userpass, decode_userpass
-from .dates import extract_datetime
