dfb323a821a35cf2e2b4c115df98af9205106def
ivandeex <ivandeex@gmail.com>
2016-05-12 23:55:30 +0300

    update requirements

--- Procfile
+++ Procfile
@@ -1,5 +1,2 @@
-web: gunicorn -w2 dvoznak.web:app --log-file -
-purge_news: python -m dvoznak.spider_news --action=purge
-crawl_news: python -m dvoznak.spider_news --action=all
-purge_tips: python -m dvoznak.spider_tips --action=purge
-crawl_tips: python -m dvoznak.spider_tips --action=all
+web: gunicorn -w2 dz.wsgi --log-file -
+crawl: python -m bot.bot --action=all
--- dz/admin.py
+++ dz/admin.py
@@ -4,39 +4,42 @@ from . import models
 admin.site.site_header = admin.site.site_title = 'DZ!'
 
 
+class DzModelAdmin(admin.ModelAdmin):
+    list_per_page = 20
+
+    def has_add_permission(self, request):
+        return False
+
+    class Media:
+        css = {'all': ['dz.css']}
+
+    def has_add_permission(self, request):
+        return False
+
+
 @admin.register(models.User)
-class UserAdmin(admin.ModelAdmin):
-    pass
+class UserAdmin(DzModelAdmin):
+    def has_add_permission(self, request):
+        import ipdb; ipdb.set_trace()
+        return True
 
 
 @admin.register(models.News)
-class NewsAdmin(admin.ModelAdmin):
+class NewsAdmin(DzModelAdmin):
     pass
 
 
 @admin.register(models.Tip)
-class TipAdmin(admin.ModelAdmin):
+class TipAdmin(DzModelAdmin):
     list_display = ['id', 'published', 'place', 'title', 'tip_block',
                     'result', 'tipster', 'coeff', 'min_coeff',
                     'stake', 'due', 'spread', 'betting', 'success',
                     'updated', 'crawled', 'details_url', 'archived']
-    class Media:
-        css = {'all': ['dz.css']}
-
-    def has_add_permission(self, request):
-        return False
 
 
 @admin.register(models.Crawl)
-class CrawlAdmin(admin.ModelAdmin):
+class CrawlAdmin(DzModelAdmin):
     list_display = ['id', 'type', 'action', 'status', 'started', 'ended',
                     'news', 'tips', 'host', 'ipaddr', 'pid']
     list_filter = ['type', 'action', 'status', 'started', 'host']
-    list_per_page = 20
     ordering = ['-id']
-
-    class Media:
-        css = {'all': ['dz.css']}
-
-    def has_add_permission(self, request):
-        return False
