7524c317e2b47392b9c85ca0ed3cdb65abb38d1e
ivandeex <ivandeex@gmail.com>
2016-08-10 14:27:37 +0300

    compile translations

--- ansible/prepare.sh
+++ ansible/prepare.sh
@@ -4,4 +4,5 @@ test secret.yml -nt group_vars/all/vault.yml && ./update-secret.sh
 ansible-playbook -i hosts task-prepare.yml
 cd ..
 python manage.py makemigrations --noinput --exit && python manage.py migrate --noinput
+python manage.py compilemessages
 python manage.py collectstatic --noinput
