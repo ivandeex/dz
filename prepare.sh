8f611f8a2c28d692821ed1a9a5c3e77118d1f69f
ivandeex <ivandeex@gmail.com>
2016-09-12 03:13:35 +0300

    restyle prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 project_dir=$(dirname $(readlink -f $0))
+
 cd $project_dir/ansible
 test secret.yml -nt group_vars/all/vault.yml && ./update-secret.sh
 ansible-playbook -i hosts task-prepare.yml
+
 cd $project_dir
-python manage.py makemigrations --noinput --exit && python manage.py migrate --noinput
+python manage.py makemigrations --noinput --exit && \
+  python manage.py migrate --noinput
 python manage.py compilemessages
+
 npm run makeprod
 python manage.py collectstatic --noinput
