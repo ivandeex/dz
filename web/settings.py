98bf0373a5a813474f0bc7cbcb3458ef7802898a
ivandeex <ivandeex@gmail.com>
2016-10-29 20:38:34 +0300

    disable common middleware

--- web/settings.py
+++ web/settings.py
@@ -108,7 +108,7 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
-    'django.middleware.common.CommonMiddleware',
+    # 'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
